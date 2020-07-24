00d4f0975369cf1ec97135cf818e3514fdab7724
Krystian Spiewak <krystianspiewak@gmail.com>
2020-07-23 19:47:22 -0500

    Tiled map created

---
+++ example_map..lua
@@ -0,0 +1,61 @@
+return {
+  version = "1.2",
+  luaversion = "5.1",
+  tiledversion = "1.3.5",
+  orientation = "orthogonal",
+  renderorder = "left-up",
+  width = 8,
+  height = 7,
+  tilewidth = 32,
+  tileheight = 32,
+  nextlayerid = 2,
+  nextobjectid = 1,
+  properties = {},
+  tilesets = {
+    {
+      name = "atlas",
+      firstgid = 1,
+      filename = "texture/atlas.tsx",
+      tilewidth = 32,
+      tileheight = 32,
+      spacing = 0,
+      margin = 0,
+      columns = 40,
+      image = "texture/atlas32.png",
+      imagewidth = 1280,
+      imageheight = 832,
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 32,
+        height = 32
+      },
+      properties = {},
+      terrains = {},
+      tilecount = 1040,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      id = 1,
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 8,
+      height = 7,
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      properties = {},
+      encoding = "base64",
+      compression = "zlib",
+      data = "eJxNzbsRQFAUhOHrWQCaEDCUImCuUiQGM6hDYtCJZ03+4AQ3+Gaj3bWUUjYcuPCM9BEjw40ciSFFAS2dGqWhQoMOPQbpZbLVYsGGHYf0tGytOHHJ/2Pkiw+BiBBixIRZ/PtCFJQ="
+    }
+  }
+}
